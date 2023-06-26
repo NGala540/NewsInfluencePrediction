76f9e9466fc60456268b845d0ae42eeb0d87e9f2
Norbert <norbertgala540@gmail.com>
2023-06-26 12:02:07 +0200

    Final version

--- app.py
+++ app.py
@@ -1,65 +1,169 @@
-# TODO: Human in the loop
-# TODO: exe file
-# TODO: work in background
 from tkinter import *
+import tkinter.messagebox
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 from app_functions import *
-import joblib
+
+
+def box_actualization(idx, news):
+    """
+    Actualize Headline of the box with news counter and body with all news data
+    :param idx: index of current displaying news
+    :param news: data frame of news
+    """
+    box_body.delete("1.0", END)
+    if len(news) > 0:
+        box_head["text"] = f"News no. {idx[0] + 1}/{len(news)}"
+        box_body.insert("1.0", print_article(news.iloc[idx[0]]))
+    else:
+        box_body.insert("1.0", "NO NEWS IN PAST 15 MINUTES")
 
 
 def next_news(idx, news):
+    """
+    Moves index to next news, checking if next button should be active
+    :param idx: index of current displaying news
+    :param news: data frame of news
+    """
     idx[0] += 1
-    box_head["text"] = f"News no. {idx[0]+1}/{len(news)}"
-    box_body.delete("1.0", END)
-    box_body.insert("1.0", print_article(news[idx[0]], model, token))
+    box_actualization(idx, news)
+    b2["state"] = "normal"
     if idx[0]+1 == len(news):
         b1["state"] = "disabled"
 
 
+def previous_news(idx, news):
+    """
+    Moves index to previous news,
+    checks if previous button should be active
+    :param idx: index of current displaying news
+    :param news: data frame of news
+    """
+    idx[0] -= 1
+    box_actualization(idx, news)
+    b1["state"] = "normal"
+    if idx[0] == 0:
+        b2["state"] = "disabled"
+
+
+def take_score(idx, news):
+    """
+    Takes score from entry box,
+    checks if it is in proper range,
+    actualize model and updates news data frame
+    :param idx: index of current displaying news
+    :param news: data frame of news
+    """
+    score = ent_score.get()
+    try:
+        if int(score) > 100:
+            score = 100
+        elif int(score) < -100:
+            score = -100
+        model_actualization(score, news.iloc[idx[0]])
+        news.loc[idx[0], 'Score'] = score
+        box_actualization(idx, news)
+    except ValueError:
+        print("Value should be a number")
+    except IndexError:
+        print("No news to assign a score to")
+    finally:
+        ent_score.delete(0, END)
+
+
 def render_box(idx, news):
-    box_head["text"] = f"News no. {idx[0]+1}/{len(news)}"
+    """
+    Organize and creates first display,
+    handles functions to button assignment and its status
+    :param idx: index of current displaying news
+    :param news: data frame of news
+    """
+    box_actualization(idx, news)
     box_head.pack()
-    box_body.insert("1.0", print_article(news[idx[0]], model, token))
-    box_body.pack(side=TOP, fill=X)
+    box_body.pack()
+
     b1["command"] = lambda: next_news(idx, news)
-    b1.pack()
+    if len(news) > 1:
+        b1["state"] = "normal"
+    else:
+        b1["state"] = "disabled"
+    b1.pack(side=RIGHT, padx=10)
 
+    b2["command"] = lambda: previous_news(idx, news)
+    b2["state"] = "disabled"
+    b2.pack(side=LEFT, padx=10)
 
-model = joblib.load('model.pkl')
-token = joblib.load('tokenizer.joblib')
-
-root = Tk()
-root.geometry("600x340")
-box_body = Text(root, width=15, height=10, wrap=WORD, spacing3=6)
-box_head = Label(root)
-b1 = Button(root, text="Next")
-
-companies_scope = ['ASSECO-POLAND', "ALE", "ALIOR-BANK", "CD-PROJEKT", "CYFROWY-POLSAT",
-                   "DNP", "JSW-JASTRZEBSKA-SPOLKA-WEGLOWA", "KGHM", "KRUK", "KETY","LPP",
-                   "MBANK", "ORANGE", "PCO", "PEKAO", "PGE", "PKN-ORLEN", "PKO", "PZU", "SPL"]
+    button_frame.pack(pady=7)
+    ent_score.pack()
+    b3["command"] = lambda: take_score(idx, news)
+    b3.pack(pady=3)
 
 
 def job_listener(event):
+    """
+    Listener of a web scrap event,
+    take scrapped news, filter current ones and adds score to them,
+    set index of current news to 0,
+    render box
+    """
     if event.exception:
         print('The job crashed :(')
     else:
-        temp = is_current(event.retval)
+        current_news = is_current(event.retval)
+        news_score = score_calculate(current_news)
+        if len(news_score) != 0:
+            show_message()
         idx = [0]
-        render_box(idx, temp)
-
-# TODO: correct appearence of new chunk of news
-if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-    scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
-    scheduler.add_job(scrapper, "interval", seconds=20, args=[companies_scope])
-    scheduler.add_job(scrapper, "date", args=[companies_scope])
-    scheduler.start()
-
-    # Shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown())
-    root.mainloop()
+        render_box(idx, news_score)
 
 
+def show_message():
+    """
+    display message when new news appear
+    """
+    tkinter.messagebox.showinfo("Let's make some money!",  "There are some new news for you")
+    root.deiconify()
+    root.attributes('-topmost', 1)
+    root.attributes('-topmost', 0)
 
+
+def on_closing():
+    """
+    save the model if it was changed
+    """
+    if changed is True:
+        joblib.dump(model, 'model.pkl')
+    root.destroy()
+
+
+# Scope of handles companies
+SCOPE = ["ASSECO-POLAND", "ALE", "ALIOR-BANK", "CD-PROJEKT", "CYFROWY-POLSAT",
+         "DNP", "JSW-JASTRZEBSKA-SPOLKA-WEGLOWA", "KGHM", "KRUK", "KETY","LPP",
+         "MBANK", "ORANGE", "PCO", "PEKAO", "PGE", "PKN-ORLEN", "PKO", "PZU", "SPL"]
+
+# main root of app declaration
+root = Tk()
+root.geometry("600x340")
+
+# widgets creation
+box_body = Text(root, width=150, height=10, wrap=WORD, spacing3=6)
+box_head = Label(root)
+button_frame = Frame(root)
+b1 = Button(button_frame, text="Next")
+b2 = Button(button_frame, text="Previous")
+ent_score = Entry(master=root, width=10)
+b3 = Button(master=root, text="Update Score")
+
+# job scheduler declaration
+scheduler = BackgroundScheduler()
+scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+# parameter minutes decide how frequent scrapper should run
+scheduler.add_job(scrapper, "interval", minutes=15, args=[SCOPE])
+scheduler.add_job(scrapper, "date", args=[SCOPE])
+scheduler.start()
+
+# save model and shut down the scheduler when exiting the app
+root.protocol("WM_DELETE_WINDOW", on_closing)
+atexit.register(lambda: scheduler.shutdown())
+root.mainloop()
--- app_functions.py
+++ app_functions.py
@@ -4,13 +4,26 @@ import pandas as pd
 from datetime import datetime, timedelta
 import numpy as np
 from keras_preprocessing.sequence import pad_sequences
+import joblib
+
+# tokenizer and model upload, variable declaration whether model was changes or not
+tokenizer = joblib.load('tokenizer.joblib')
+model = joblib.load('model.pkl')
+changed = False
+
 
 def scrapper(companies):
-    attributes_container = {"Company": [], "Author": [], "Headline": [], "Text": [], "Date": []}
+    """
+    Scraps news from first news-page of company profile on https://www.biznesradar.pl/
+    :param companies: list of companies about which news are going to be scrapped
+    :return: dict of news data
+    """
+    attributes_container = {"Company": [], "Author": [],"Headline": [], "Text": [], "Date": []}
     for company in companies:
         url_br = f'https://www.biznesradar.pl/wiadomosci/{company}'
         page_br = urllib.request.urlopen(url_br).read()
         soup_br = BeautifulSoup(page_br, 'html.parser')
+        # iterating through relevant html objects
         for elem in soup_br.find_all(['div', 'span']):
             if elem.name == 'div':
                 try:
@@ -37,30 +50,71 @@ def scrapper(companies):
 
 
 def is_current(attributes_container):
-    results = []
+    """
+    Filters recent news only
+    parameter minutes in timedelta decide how recent news should be
+    :param attributes_container: dict of news data
+    :return: DataFrame of most recent news
+    """
     df = pd.DataFrame(attributes_container)
     df['Date'] = pd.to_datetime(df['Date'])
-    # TODO: Iterating through pandas objects is generally slow. In many cases, iterating manually over the rows is not needed
-    for index, row in df.iterrows():
-        if row['Date'] >= datetime.now() - timedelta(minutes=90):
-            results.append(row)
-
+    results = df[df['Date'] >= (datetime.now() - timedelta(minutes=15))]
+    results.sort_values(by=['Date'], inplace=True, ascending=False)
+    results.reset_index(inplace=True, drop=True)
     return results
 
-def print_article(article, model, tokenizer):
-    corpus = input_processor(str(article[2]), str(article[3]), tokenizer)
+
+def score_calculate(articles):
+    """
+    Calculate score of each news
+    :param articles: DataFrame of most recent news
+    :return: DataFrame of most recent news with score
+    """
+    articles = articles.assign(Score=lambda rows:
+        [round(model.predict(j)[0][0] * 100) for j in [article_preprocessing(rows.iloc[i]) for i in range(len(rows))]])
+    return articles
+
+
+def print_article(article):
+    """
+    creates a string to put in text widget
+    :param article: a single article from DataFrame
+    :return: string do display
+    """
     result = \
-    f'Company: {str(article[0])}\n' +\
-    f'Author: {str(article[1])}\n' +\
-    f'Headline: {str(article[2])}\n' +\
-    f'Text: {str(article[3])}\n' +\
-    f'Date: {str(article[4])}\n' +\
-    f'Score: {round(model.predict([corpus])[0][0] * 100)} \n'
+    f'Company: {article["Company"]}\n' +\
+    f'Author: {article["Author"]}\n' +\
+    f'Headline: {article["Headline"]}\n' +\
+    f'Text: {article["Text"]}\n' + \
+    f'Date: {article["Date"]}\n' +\
+    f'Score: {article["Score"]} \n'
     return result
 
-def input_processor(head, body, tokenizer):
-    concat = head + " " + body
+
+def model_actualization(score, article):
+    """
+    fit the model on provided article and score
+    :param score: string representation of target value for given article
+    :param article: single lie from DataFrame representing an article
+    :return:
+    """
+    model_input = article_preprocessing(article)
+    target = np.asarray(int(score)/100).astype('float32')
+    model.fit(model_input, np.array([target]), epochs=3, verbose=0)
+    # changing variable to trace model change
+    global changed
+    changed = True
+
+
+def article_preprocessing(article):
+    """
+    maps article to a vector so that it would suit the model input
+    :param article: single lie from DataFrame representing an article
+    :return: vector of values representing an article
+    """
+    concat = article["Headline"] + " " + article["Text"]
     tokenized = tokenizer.texts_to_sequences([concat])
     padded = pad_sequences(tokenized, maxlen=100)
-    result = np.asarray(padded).astype('float32')
-    return result
+    model_input = np.asarray(padded).astype('float32')
+    return model_input
+
